bf596512bbf57ccce50c2f29ecf64300a8466254
yang_xiaojian <yang_xiaojian@qq.com>
2021-04-07 17:23:56 +0800

    maven modules example

---
+++ upc/src/test/java/com/xiaojian/upc/UpcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xiaojian.upc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpcApplicationTests {
+
+    @Test
+    void contextLoads() {
+        int a1 =0;
+        int a2 =0;
+        int a3 =0;
+    }
+
+}
